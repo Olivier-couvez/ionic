2eb695dbd911d6b1fd9f6e4cd31b660339790530
Couvez olivier <o.couvez@free.fr>
2021-11-02 17:09:41 +0100

    TEST

--- nbmysterenew/src/app/parametres/parametres.page.ts
+++ nbmysterenew/src/app/parametres/parametres.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-parametres',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parametres.page.scss'],
 })
 export class ParametresPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() valmax: string;
+  @Input() mesEssais: string;
+  parametres: Param;
+  essais: number;
+  max: number;
+  constructor(public modalCtrl: ModalController) {
+    this.parametres = new Param();
   }
 
+  ngOnInit() {}
+  dismiss() {
+    this.modalCtrl.dismiss(this.parametres);
+  }
+
+  setNbEssais(_essais) {
+    this.parametres.nbEssais = _essais;
+  }
+  setMax(_max) {
+    this.parametres.valMax = _max;
+  }
+}
+
+class Param {
+  nbEssais: number;
+  valMax: number;
+  constructor() {
+    this.nbEssais = 0;
+    this.valMax = 0;
+  }
 }
